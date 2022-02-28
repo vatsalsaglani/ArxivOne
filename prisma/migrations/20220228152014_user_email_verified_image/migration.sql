7ca0f5a4bddcc9f403ab551ec2ceefdf25f1d033
vatsals <youknowiamtrying@gmail.com>
2022-02-28 21:37:39 +0530

    added changes to the schema

---
+++ prisma/migrations/20220228152014_user_email_verified_image/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `image` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "emailVerified" TIMESTAMP(3),
+ADD COLUMN     "image" TEXT NOT NULL;
