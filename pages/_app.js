fdb6b26c7fff738e6311e2ed3fb0c979202be2d9
vatsals <youknowiamtrying@gmail.com>
2022-03-01 18:49:50 +0530

    wrapped global context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { GlobalContextProvider } from "../contexts/global";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+      <GlobalContextProvider>
+        <Component {...pageProps} />
+      </GlobalContextProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+export default MyApp;
