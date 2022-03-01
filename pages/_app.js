db65d34ba992c9af1415440cbe3d7b1367853d66
vatsalsaglani <saglanivatsal@gmail.com>
2022-03-02 00:24:28 +0530

    added theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { GlobalContextProvider } from "../contexts/global";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider>
-      <GlobalContextProvider>
-        <Component {...pageProps} />
-      </GlobalContextProvider>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <GlobalContextProvider>
+          <Component {...pageProps} />
+        </GlobalContextProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
