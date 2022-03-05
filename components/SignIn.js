80c4b7e378886690493a109f0222bcabbb49a660
vatsalsaglani <saglanivatsal@gmail.com>
2022-03-05 11:34:06 +0530

    changed signin form made it a component

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,61 +1,95 @@
 import { useEffect, useState } from "react";
-import {
-  useSession,
-  signIn,
-  signOut,
-  getProviders,
-  getSession,
-} from "next-auth/react";
-import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
 
-export default function SignInComponent({ providers }) {
-  const { data: session } = useSession();
-  const router = useRouter();
+// components
+import { Icon } from "./Navigation";
 
-  useEffect(() => {
-    if (session) {
-      router.push("/home");
-    }
-  }, []);
+// icons
+import { MdAlternateEmail } from "react-icons/md";
+import { AiOutlineEye, AiFillGithub, AiFillEyeInvisible } from "react-icons/ai";
 
-  if (session) {
-    return <div></div>;
-  } else {
-    return (
-      <div className="my-10 flex h-fit w-full flex-col items-center justify-center py-2.5">
-        {providers
-          ? Object.values(providers).map((provider) => (
-              <button
-                key={provider.id}
-                className={
-                  "my-8 w-80 rounded-full bg-gradient-to-r from-green-400 to-blue-500 p-4 hover:from-blue-500 hover:to-green-600"
-                }
-                onClick={() => {
-                  signIn(provider.id);
-                }}
-              >
-                Sign in with {provider.name}
-              </button>
-            ))
-          : null}
-      </div>
-    );
-  }
-}
-
-export async function getServerSideProps(context) {
-  const { req } = context;
-  const session = await getSession({ req });
-  console.log("SESSION: ", session);
-  if (session) {
-    return {
-      redirect: { destination: "/home" },
+export default function SignInComponent() {
+  const [passwordText, setPasswordText] = useState("password");
+  const onClickEye = () => {
+    const stateChange = {
+      text: "password",
+      password: "text",
     };
-  }
-
-  return {
-    props: {
-      providers: await getProviders(context),
-    },
+    setPasswordText(stateChange[passwordText]);
   };
+  return (
+    <div className="mx-auto flex min-h-screen min-w-full flex-col items-center justify-center">
+      <div className="relative h-full w-full max-w-lg">
+        <div className="absolute top-0 -left-5 h-96 w-96 animate-blob rounded-full bg-pink-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+        <div className="animation-delay-2000 absolute top-0 -right-5 h-96 w-96 animate-blob rounded-full bg-yellow-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+        <div className="animation-delay-4000 absolute -bottom-10 left-20 h-96 w-96 animate-blob rounded-full bg-pink-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+        <div className="relative flex flex-col items-center justify-center sm:px-6 lg:px-8">
+          <h1 className="mb-2 text-center text-2xl font-bold text-sky-600 sm:text-4xl">
+            Welcome Back
+          </h1>
+          <form
+            action=""
+            className={
+              "mt-6 mb-0 min-w-fit space-y-4 rounded-lg p-20 shadow-2xl"
+            }
+          >
+            <p className="text-xl font-medium text-sky-600 ">
+              Sign in to your account
+            </p>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-500"
+              >
+                Email
+              </label>
+              <div className="relative mt-1">
+                <input
+                  type="email"
+                  id="email"
+                  className="text-md w-full rounded-lg border-gray-200 p-4 pr-12 shadow-sm"
+                  placeholder="Enter Email"
+                />
+                <span className="absolute inset-y-0 right-4 inline-flex items-center">
+                  <Icon icon={<MdAlternateEmail size={16} />} />
+                </span>
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-500"
+              >
+                Password
+              </label>
+              <div className="relative mt-1">
+                <input
+                  type={passwordText}
+                  id="password"
+                  className="text-md w-full rounded-lg border-gray-200 p-4 pr-12 shadow-sm"
+                  placeholder="Enter Password"
+                />
+                <span
+                  className="absolute inset-y-0 right-4 inline-flex items-center"
+                  onClick={() => {
+                    onClickEye();
+                  }}
+                >
+                  <Icon
+                    icon={
+                      passwordText === "password" ? (
+                        <AiFillEyeInvisible size={16} />
+                      ) : (
+                        <AiOutlineEye size={16} />
+                      )
+                    }
+                  />
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
