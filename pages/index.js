18d1358e043d215d7d4dc5195c54acd80dadf1e5
vatsalsaglani <saglanivatsal@gmail.com>
2022-03-06 13:41:38 +0530

    updated link to signin page

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
               power of AI
             </p>
             <div className="mt-8 flex flex-wrap justify-center gap-4 font-Work">
-              <Link href="/account">
+              <Link href="/signin">
                 <a className="block w-full rounded border border-sky-600 bg-sky-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
                   Get Started
                 </a>
